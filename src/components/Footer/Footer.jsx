bacd62a6c37b5f5f41df577682eee79f057635dd
nightorb <ana.nath@gmx.de>
2022-06-03 00:37:57 +0200

    fix footer layout for mobile devices

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,20 +5,22 @@ import './Footer.scss';
 export default function Footer() {
   return (
     <footer id="Footer">
-      <div className="footer-text">
-        <p>Don't forget to add me on my social media</p>
+      <div className="footer">
+        <div className="footer-text">
+          <p>Don't forget to add me on my social media</p>
+        </div>
+        <div className="footer-links">
+          <a href="https://www.linkedin.com/in/anastasia-n-945661227" className="footer-icons" target="_blank" rel="noreferrer">
+            <img src={process.env.PUBLIC_URL + "/icons/linkedin_icon.svg"} alt="LinkedIn logo" />          
+          </a>
+          <a href="https://github.com/nightorb" className="footer-icons" target="_blank" rel="noreferrer">
+            <img src={process.env.PUBLIC_URL + "/icons/github_icon.svg"} alt="GitHub logo" />
+          </a>
+          <a href="https://www.artstation.com/nightorb" className="footer-icons" target="_blank" rel="noreferrer">
+            <img src={process.env.PUBLIC_URL + "/icons/artstation_icon.svg"} alt="Artstation logo" />
+          </a>
+        </div>
       </div>
-      <nav className="footer-nav">
-        <a href="https://www.linkedin.com/in/anastasia-n-945661227" className="footer-icons" target="_blank" rel="noreferrer">
-          <img src={process.env.PUBLIC_URL + "/icons/linkedin_icon.svg"} alt="LinkedIn logo" />          
-        </a>
-        <a href="https://github.com/nightorb" className="footer-icons" target="_blank" rel="noreferrer">
-          <img src={process.env.PUBLIC_URL + "/icons/github_icon.svg"} alt="GitHub logo" />
-        </a>
-        <a href="https://www.artstation.com/nightorb" className="footer-icons" target="_blank" rel="noreferrer">
-          <img src={process.env.PUBLIC_URL + "/icons/artstation_icon.svg"} alt="Artstation logo" />
-        </a>
-      </nav>
       <div className="footer-copyright">
         <p>Designed & Built by Anastasia Nathanailidou</p>
       </div>
